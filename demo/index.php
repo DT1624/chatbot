ac57681045b71e93a0cc7fcf701bc534ae8f5e89
Dương Trần <anhduonghctdvp@gmail.com>
2024-02-19 23:28:05 +0700

    Add files via upload

---
+++ demo/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <h1></h1>
+    </div>
+    <div>
+        <form action="api.php" method = "post">
+            <div>
+                <input type = "text" name = "prompt">
+            </div>
+            <br>
+            <div>
+                <input type = "submit" value = "Generate" />
+            </div>
+        </form>
+    </div>
+
+</body>
+
+</html>
